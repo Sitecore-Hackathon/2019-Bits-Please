59682a23accbfe85aaf8cda8b19764630a669ede
wmoyano <g.moyano91@gmail.com>
2019-03-02 11:23:03 -0500

    Added Controller
    Added Controller

---
+++ src/Feature/UserXData/code/Controllers/UserXDataController.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.UserXData.Controllers
+{
+    public class UserXDataController : SitecoreController
+    {
+
+    }
+}
